51e270ac019ddd46dd7adf36e836bd1a13c26d8b
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-06-16 12:28:00 +0100

    Updated script

--- mysqlstaging/staging.sh
+++ mysqlstaging/staging.sh
@@ -15,4 +15,4 @@ for file in *.sql.gz ; do
   fi
 done  
 
-mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD"' < $sql
+mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" < $sql
