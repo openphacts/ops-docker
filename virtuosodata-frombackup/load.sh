a8f99e1031280c600de3e288a14f4828f117261d
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-06-16 12:16:17 +0100

    debug - how many files to load?

--- virtuosodata-frombackup/load.sh
+++ virtuosodata-frombackup/load.sh
@@ -8,7 +8,8 @@ if ! [ -f *#1.bp ] ; then
   /usr/local/bin/download.sh	
 fi
 DUMP=`echo *#1.bp | sed s/1.bp//`
-echo Loading $DUMP
+echo Loading $DUMP -- `find -type f| wc -l` files
+
 virtuoso-t +configfile /etc/virtuoso-opensource-7/virtuoso.ini  +restore-backup $DUMP
 rm $DUMP*
 touch .loaded
