9a7136c4f518e13bb232fafd9b6575928437fe44
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-06-17 03:13:23 +0100

    use axel for download of sqldump

--- mysqlstaging/download.sh
+++ mysqlstaging/download.sh
@@ -6,10 +6,10 @@ NAME=ims-1.5-20150519.sql.gz
 url=${BASE}${NAME}
 
 rm -f *.sha1
-wget --no-verbose $url.sha1 
+axel --quiet -n 1 $url.sha1 
 
 if ! [ -f $NAME ] || ! sha1sum --status -c $NAME.sha1 2>/dev/null ; then
-  wget --no-verbose $url
+  axel --alternate $url
 fi
 sha1sum -c *.sha1
 
