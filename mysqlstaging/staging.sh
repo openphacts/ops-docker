c6f592aa77ba5157178309a5eeab378a9d997069
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-06-24 11:16:31 +0100

    more robust staging

--- mysqlstaging/staging.sh
+++ mysqlstaging/staging.sh
@@ -1,20 +1,44 @@
 #!/bin/sh
+set -e
+# wait for mysql
+sleep 5 &
 
-sql=/tmp/staging-$$.sql
+sql=/tmp/staging.sql
 rm -f $sql
+# empty file to start with
+touch sql
 
 cd /staging
+# Forget about any incomplete staging
+rm -rf staging
+mkdir -p staging staged
 for file in *.sql.gz ; do
   db=`echo $file | sed 's/\\.gz$//' | sed 's/\\.sql//'`
-  staged=.$db.staged
+  staged=staged/$db
+  staging=staging/$db
   if ! [ -f $staged ] ; then
-    echo "Staging $db"
+    echo "Preparing to stage $db"
     echo "CREATE DATABASE IF NOT EXISTS $db;" >> $sql
     echo "USE $db;" >> $sql
     gunzip --stdout $file >> $sql
-    touch $staged
+    touch $staging
   fi
 done  
 
-mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" < $sql
+# To avoid password warnings..
+echo "[client]" > /tmp/my.conf
+echo "host=$MYSQL_PORT_3306_TCP_ADDR" >> /tmp/my.conf
+echo "port=$MYSQL_PORT_3306_TCP_PORT" >> /tmp/my.conf
+echo "user=root" >> /tmp/my.conf
+echo "password=$MYSQL_ENV_MYSQL_ROOT_PASSWORD" >> /tmp/my.conf
+
+# ensure at least 5 seconds has passed
+echo "Waiting for mySQL"
+wait
+echo "mySQL staging"
+ls -alh /tmp/staging.sql
+
+mysql --defaults-file=/tmp/my.conf < $sql
+# Mark as staged
+mv staging/* staged/
 echo "mySQL staging finished"
