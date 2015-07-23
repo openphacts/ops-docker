3243df05ad91a2f51c5885fdc5d9fdac1630683a
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-07-23 17:16:56 +0100

    virtuoso restore from _1.bp instead of #_1.bp

--- virtuosodata-frombackup/load.sh
+++ virtuosodata-frombackup/load.sh
@@ -4,10 +4,11 @@ cd /virtuoso
 if [ -f .loaded ] ; then
   exit
 fi
-if ! [ -f *#1.bp ] ; then
+if ! [ -f *_1.bp ] ; then
   /usr/local/bin/download.sh	
 fi
-DUMP=`echo *#1.bp | sed s/1.bp//`
+# FIXME: Check for _1.bp vs _#1.bp
+DUMP=`echo *_1.bp | sed s/1.bp//`
 echo Loading $DUMP -- `find -type f| wc -l` files
 
 virtuoso-t +configfile /etc/virtuoso-opensource-7/virtuoso.ini  +restore-backup $DUMP
