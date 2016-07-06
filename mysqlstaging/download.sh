2161e02151253a0c9bfba9d84e994faec571f5b4
Stian Soiland-Reyes <stain@apache.org>
2016-07-06 13:02:37 +0100

    Use wget instead of axel
    and modify sha1 checksums.
    
    This fixes #2

--- mysqlstaging/download.sh
+++ mysqlstaging/download.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 set -e
-BASE=https://data.openphacts.org/commercial/2.0/ims/
+BASE=https://data.openphacts.org/free/2.0/ims/
 NAME=mysql-for-ims-2.0-20151214.sql.gz
 
 url=${BASE}${NAME}
 
 rm -f *.sha1
-axel --quiet -n 1 $url.sha1 
+#axel --alternate $url.sha1 
+wget $url.sha1 
 
 if ! [ -f $NAME ] || ! sha1sum --status -c $NAME.sha1 2>/dev/null ; then
-  axel --alternate $url
+  #axel --alternate $url
+  wget $url
 fi
-sha1sum -c *.sha1
+
+name=`echo *.sha1 | sed s/.sha1//`
+(cat *.sha1 ; echo "  $name") | sha1sum -c  -
 
