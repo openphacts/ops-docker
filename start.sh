fa6149ff316f8b60d2e51ad80dfeb0e02cc4ae3a
Randy Kerber <randy@randykerber.com>
2016-11-06 14:19:20 -0600

    Add a script to start containers to manually bypass docker-compose.

--- start.sh
+++ start.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # DC_HOME=$(pwd)
-DC_HOME=/media/tyree/ops/prj/ops-docker
+export DC_HOME=/media/tyree/ops/prj/ops-docker
 
 cd ${DC_HOME}/openphacts-rdf
 
-OPSRDF_CID=$( docker create \
+export OPSRDF_CID=$( docker create \
                      --name ops-rdf \
                      -v /media/tyree/ops/rdf/download/:/download/ \
     )
 
 cd ${DC_HOME}/virtuosodata-fromrdf
 
-OPSSTG_CID=$( docker create \
+export OPSSTG_CID=$( docker create \
                      --name ops-virt-staging \
                      --volumes-from ${OPSRDF_CID} \
                      -v /media/tyree/ops/rdf/staging/:/staging/ \
