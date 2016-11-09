80fe958b3650ef865de7b792f54324838027c769
Randy Kerber <randy@randykerber.com>
2016-11-08 19:09:31 -0500

    Try fix docker can’t find /staging/staging.sql. Move cp to run.

--- virtuosodata-fromrdf/load.sh
+++ virtuosodata-fromrdf/load.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-if [ -f /staging/.staged ] ; then
-  echo "RDF is already loaded. To start again, try:"
-  echo "    docker-compose run virtuosostaging rm /staging/.staged"
-  exit 0
-fi
+# if [ -f /staging/.staged ] ; then
+#   echo "RDF is already loaded. To start again, try:"
+#   echo "    docker-compose run virtuosostaging rm /staging/.staged"
+#   exit 0
+# fi
+
+cp /staging.sql /staging/
 
 echo 'Loading RDF from /staging'
 tail -F /virtuoso/virtuoso.log &
@@ -18,13 +20,13 @@ ls -ld /usr/local/bin/isql
 
 echo 'ls -ld /usr/local/bin/staging.sh'
 ls -ld /usr/local/bin/staging.sh
-which staging.sh
+# which staging.sh
 
-staging.sh
+/usr/local/bin/staging.sh
 
 # isql 1111 dba dba /staging/staging.sql
 # rm /staging/*
 
-touch /staging/.staged
+# touch /staging/.staged
 
 echo 'Loading finished'
