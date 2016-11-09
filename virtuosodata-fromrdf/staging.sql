fd6aa4f2d368fc590fffecb3d2f03c8b367f5d3e
Randy Kerber <randy@randykerber.com>
2016-11-08 21:56:57 -0500

    Enable conceptwiki and wikipathways.

--- virtuosodata-fromrdf/staging.sql
+++ virtuosodata-fromrdf/staging.sql
@@ -37,8 +37,8 @@
 -- -- ChEBI
 -- ld_dir('/staging/chebi/data' , '*.owl.gz' , 'http://www.ebi.ac.uk/chebi' );
 
--- -- ConceptWiki
--- ld_dir('/staging/CW' , '*.nq.gz' , 'http://www.conceptwiki.org' );
+-- ConceptWiki
+ld_dir('/staging/conceptwiki/data/' , '*.ttl.gz' , 'http://www.conceptwiki.org' );
 
 -- -- DisGeneT
 -- ld_dir('/staging/disgenet', '*.nq.gz', 'http://rdf.imim.es');
@@ -54,15 +54,51 @@
 
 
 
-
-
 -- -- VoID Dataset Descriptors
 -- ld_dir('/staging/void' , '*.nq.gz' , 'http://www.openphacts.org/api/datasetDescriptors' );
 
--- -- WikiPathways
--- ld_dir('/staging/WP' , '*.nq.gz' , 'http://www.wikipathways.org' );
--- ld_dir('/staging/WP/WPREACTRDF' , '*.nq.gz' , 'http://www.wikipathways.org' );
--- ld_dir('/staging/WP/OPSWPRDF' , '*.nq.gz' , 'http://www.wikipathways.org' );
+
+
+-- WikiPathways
+ld_dir('/staging/wikipathways/data' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+
+ld_dir('/staging/wikipathways/data/gpml/Dog' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Rice' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Fruit_fly' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Arabidopsis_thaliana' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Zebra_fish' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Yeast' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Mouse' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Maize' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Escherichia_coli' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Worm' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Mosquito' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Bacillus_subtilis' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Human' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Cow' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Chicken' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Chimpanzee' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Rat' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/gpml/Fusarium_graminearum' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+
+ld_dir('/staging/wikipathways/data/wp/Dog' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Rice' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Fruit_fly' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Arabidopsis_thaliana' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Zebra_fish' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Yeast' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Mouse' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Maize' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Escherichia_coli' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Worm' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Mosquito' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Bacillus_subtilis' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Human' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Cow' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Chicken' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Chimpanzee' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Rat' , '*.ttl.gz' , 'http://www.wikipathways.org' );
+ld_dir('/staging/wikipathways/data/wp/Fusarium_graminearum' , '*.ttl.gz' , 'http://www.wikipathways.org' );
 
 
 rdf_loader_run();
