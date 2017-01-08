0d573ee25993fcb78ff6f5736775bc7bab42fb80
Randy Kerber <randy@randykerber.com>
2017-01-08 15:10:46 -0600

    Fix & enhance script for running ‘insert_queries.sparql’ files.

--- virtuosodata-fromrdf/inferences.sh
+++ virtuosodata-fromrdf/inferences.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-isql 1111 dba dba /staging/enzyme/data/insert_queries.sparql
-isql 1111 dba dba /staging/chembl/data/insert_queries.sparql
-isql 1111 dba dba /staging/bao/data/insert_queries.sparql
-isql 1111 dba dba /staging/go/data/insert_queries.sparql
-isql 1111 dba dba /staging/disgenet/data/insert_queries.sparql
-isql 1111 dba dba /staging/chebi/data/insert_queries.sparql
+declare -a datasets=(
+    "enzyme"
+    "chembl"
+    "bao"
+    "go"
+    "disgenet"
+    "chebi"
+)
+
+function run_insert_query() {
+    local insert_query_file="$1"
+
+    echo "execute SPARQL INSERT query in file: ${insert_query_file}"
+
+    (echo "SPARQL"; cat "${insert_query_file}"; echo ";") | isql 1111 dba dba
+}
+
+for dataset in "${datasets[@]}"
+do
+    run_insert_query "/staging/${dataset}/data/insert_queries.sparql"
+done
+
 
 isql 1111 dba dba exec="rdf_loader_run();"
