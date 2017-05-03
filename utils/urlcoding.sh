37bfb1d728105ec9fecb7ac30fa56208be8dd561
Randy Kerber <randy@randykerber.com>
2017-05-03 16:24:14 -0400

    Utility bash functions for URL encode and decode.

---
+++ utils/urlcoding.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+## Functions to perform URL encoding and decoding of URLs passed to HTTP web services.
+
+urlencode() {
+    # urlencode <string>
+    old_lc_collate=$LC_COLLATE
+    LC_COLLATE=C
+
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+
+    LC_COLLATE=$old_lc_collate
+}
+
+urldecode() {
+    # urldecode <string>
+
+    local url_encoded="${1//+/ }"
+    printf '%b' "${url_encoded//%/\\x}"
+}
